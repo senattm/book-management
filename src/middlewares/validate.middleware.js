c37e302fb6d83e586ea1b058cf3f4f2b777300f7
senattm <senaatim95@hotmail.com>
2025-12-19 10:41:44 +0300

    feat: add request validation middleware

---
+++ src/middlewares/validate.middleware.js
@@ -0,0 +1,23 @@
+const validate = (schema) => {
+  return (req, res, next) => {
+    try {
+      schema.parse({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+      next();
+    } catch (error) {
+      const err = new Error("Geçersiz input");
+      err.statusCode = 400;
+      err.code = "VALIDATION_ERROR";
+      err.details = error.errors.map((e) => ({
+        field: e.path.join("."),
+        message: e.message,
+      }));
+      next(err);
+    }
+  };
+};
+
+module.exports = validate;
