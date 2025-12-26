012f48098939d60179441a175ea34524b3474330
senattm <senaatim95@hotmail.com>
2025-12-26 12:51:27 +0300

    fix: remove unnecessary route

--- src/routes/v1/reviews.routes.js
+++ src/routes/v1/reviews.routes.js
@@ -1,71 +1,5 @@
 /**
  * @swagger
- * /api/v1/reviews/{id}/reviews:
- *   get:
- *     tags:
- *       - Reviews
- *     summary: Kitaba ait yorumları getir
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *           format: uuid
- *         description: Kitap ID
- *       - in: query
- *         name: page
- *         schema:
- *           type: integer
- *           default: 1
- *         description: Sayfa numarası
- *       - in: query
- *         name: limit
- *         schema:
- *           type: integer
- *           default: 10
- *         description: Sayfa başına yorum sayısı
- *     responses:
- *       200:
- *         description: Yorumlar başarıyla getirildi
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       id:
- *                         type: string
- *                       rating:
- *                         type: integer
- *                         minimum: 1
- *                         maximum: 5
- *                       comment:
- *                         type: string
- *                       userid:
- *                         type: string
- *                       bookid:
- *                         type: string
- *                       createdat:
- *                         type: string
- *                         format: date-time
- *                       updatedat:
- *                         type: string
- *                         format: date-time
- *                 pagination:
- *                   type: object
- *       404:
- *         description: Kitap bulunamadı
- *       400:
- *         description: Geçersiz ID formatı
- */
-
-/**
- * @swagger
  * /api/v1/reviews/{id}:
  *   put:
  *     tags:
@@ -163,9 +97,6 @@ const {
   reviewIdParamSchema,
   updateReviewSchema,
 } = require("../../validators/reviews.validator");
-const bookIdParamSchema = require("../../validators/book.validator");
-
-router.get("/:id/reviews", validate(bookIdParamSchema), reviewController.getReviewsByBook);
 
 router.put(
   "/:id",
